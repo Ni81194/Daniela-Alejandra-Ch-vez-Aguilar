41e41a4cc654ca44a30ea1b4234788dda6c0c641
Ni81194 <67292148+Ni81194@users.noreply.github.com>
2021-03-26 12:20:52 -0500

    Add files via upload

---
+++ escitala.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <string.h>
+using namespace std;
+class algoritmoX{
+private: 
+   int clave=63;
+public:
+   string cifrado(string & mensaje){ 
+     string mensajeC=mensaje+ "  ";
+     mensaje="";
+      for (int i=0; i< clave/10; i++){
+        for (int j=0; j< clave%10; j++){
+        mensaje= mensaje+mensajeC[j*(clave/10)+i]; 
+          }
+      }    
+     return mensaje;
+   }
+   string decifrado(string & mensajeC){ 
+     string mensajeO="";
+      for (int i=0; i<clave%10; i++){
+        for (int j=0; j< clave/10; j++){
+        mensajeO= mensajeO + mensajeC[j*(clave%10)+i]; 
+      }
+      }
+     return mensajeO;
+   }
+};
+int main() {
+  algoritmoX Emisor; 
+  algoritmoX Receptor;
+  string mensaje = "hola como estAn";
+  cout<<"Mensaje= "<<mensaje <<endl;
+  string mensajeCifrado= Emisor.cifrado(mensaje); 
+  cout<<"Mensaje Cifrado= "<< mensajeCifrado <<endl;
+  string mensajeOriginal= Receptor.decifrado(mensajeCifrado);
+  cout<<"Mensaje original= "<<mensajeOriginal<<endl;
+}
